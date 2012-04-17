b6aa2c6b8f28b5341aa51658d673f42052b637c3
fauzan ahsan <fauzan.ahsan@gmail.com>
2012-04-17 13:32:09 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metalk::Application.config.secret_token = '6949db381d5672fd8bd8e2af819f63bfca6612cbce953a1b1f02b1a6f3929627456b9eed38fff3193e89bcd4f7b237bc2ba70217c612626dcd23f9f5207adcf3'
